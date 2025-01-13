3d3a1b539952079a55149e0de1cf0c6ee945578e
Hunor17vok <163414670+Hunor17vok@users.noreply.github.com>
2025-01-13 09:40:38 +0100

    2

---
+++ Kalapácsvetést/Sportolo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalapácsvetést
+{
+    internal class Sportolo
+    {
+        public int Helyezés { get; set; }
+        public string Eredmyény { get; set; }
+        public string Sportolok { get; set; }
+        public string Orszagkod { get; set; }
+        public string Hejszin { get; set; }
+        public string Dátum { get; set; }
+        public Sportolo(string sor)
+        {
+            string[] s = sor.Split(';');
+            Helyezés = int.Parse(s[0]);
+            Eredmyény = s[1];
+            Sportolok = s[2];
+            Orszagkod = s[3];
+            Hejszin = s[4];
+            Dátum = s[5];
+        }
+    }
+}
