c81a7e7d68c75d8ac1cc7848be74740b3e950c6c
Hunor17vok <163414670+Hunor17vok@users.noreply.github.com>
2025-01-13 09:30:30 +0100

    Add project files.

---
+++ Kalapácsvetést/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kalapácsvetést
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
